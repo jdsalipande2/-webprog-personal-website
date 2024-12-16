3feb162b4e0d124524e72086774836603e231a4f
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2024-12-16 14:40:38 +0800

    updated

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -3,7 +3,7 @@ function myFunction() {
   }
 
   function myFunction2() {
-    const element = document.getElementsByTagName("h2");
+    const element = document.getElementsByTagName("p");
     document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
   
   }
